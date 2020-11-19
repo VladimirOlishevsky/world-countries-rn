a4ea438425f95a25011d4d50a0035de0696e9be4
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2020-11-19 23:30:06 +0400

    add banner,add some styles, delete non usage depends

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -8,65 +8,67 @@ import {
 } from 'react-native';
 
 import React from 'react';
-import { Typography } from '@material-ui/core'
+import { Typography, Box, Grid, Paper, Divider, withStyles } from '@material-ui/core'
 import { styles } from './rawStyles';
 import { title } from './config';
+
 
 declare function require(path: string): any;
 const themePic = require('../../assets/images/theme.jpg');
 
+const pic1 = require('../../assets/images/1.jpg')
+const pic2 = require('../../assets/images/2.jpg')
+const pic3 = require('../../assets/images/3.jpg')
+const pic4 = require('../../assets/images/4.jpg')
+
 export const Home = () => {
+
+    //const styles = useStyles();
     return (
         <ScrollView>
             <View
-                style={styles.imageContainer}>
+            style={styles.imageContainer}
+            >
                 <Image source={themePic}
                     style={styles.image}
                 />
             </View>
             <View style={styles.titleBlock}>
-                <Text style={styles.title} > {title.toUpperCase()}</Text>
+                <Text> {title.toUpperCase()}</Text>
             </View>
+            <View style={styles.container}>
+                <View>
+                    <View >
+                        <Text>
+                            Some Facts about Africa
+                    </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Africa is the second-largest continent in the world in both area and population.
+                            It is an almost entirely isolated landmass with only a small land bridge in the northeast,
+                            connecting the African Mainland with Western Asia.
+                        </Text>
+                    </View>
+                </View>
+                {/* <Divider style={styles.divider} /> */}
+                <View>
+                    <View>
+                        <Text
+                           
+                        >
+                            How many countries are there in Africa?
+                    </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            48 countries share the area of mainland Africa, plus six island nations are considered to be part of the continent.
+                            All in all, there are 54 sovereign African countries and two disputed areas, namely Somaliland and Western Sahara
+                            (see the list of African countries below).
+                        </Text>
+                    </View>
+                </View>
 
-
-
-            <View>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex sed quidem quisquam iusto neque, placeat saepe omnis. Maxime, ea libero porro ipsam quaerat veritatis magni molestias sequi veniam inventore.
-                </Text>
             </View>
         </ScrollView>
     )
--- src/Home/rawStyles.ts
+++ src/Home/rawStyles.ts
@@ -1,17 +1,10 @@
 import { StyleSheet, Dimensions } from "react-native";
+import { makeStyles } from '@material-ui/core/styles';
 
 const win = Dimensions.get('screen');
-const ratio = win.width / 541;
 
 export const styles = StyleSheet.create({
-    imageContainer: {
-        flexGrow: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        
-    },
     image: {
-        //flex: 1,
         flexGrow: 1,
         alignSelf: 'stretch',
         width: win.width,
@@ -20,16 +13,64 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "column",
-        justifyContent: 'center'
+        justifyContent: 'center',
+        padding: 30,
+        backgroundColor: 'rgba(236, 161, 29, 0.4)'
+    },
+    root: {
+        flexGrow: 1,
+    },
+    imageContainer: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+
     },
     titleBlock: {
         position: 'absolute',
         width: '100%',
         textAlign: 'center',
-        top: '10%'
+        top: '15%'
     },
     title: {
         fontSize: 40,
         top: 0,
+    },
+    divider: {
+        marginTop: 20,
+        marginBottom: 20
     }
 });
+
+// export const useStyles = makeStyles((theme) => ({
+//     container: {
+//         flex: 1,
+//         flexDirection: "column",
+//         justifyContent: 'center',
+//         padding: 30,
+//         backgroundColor: 'rgba(236, 161, 29, 0.4)'
+//     },
+//     root: {
+//         flexGrow: 1,
+//     },
+//     imageContainer: {
+//         flexGrow: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+
+//     },
+//     titleBlock: {
+//         position: 'absolute',
+//         width: '100%',
+//         textAlign: 'center',
+//         top: '15%'
+//     },
+//     title: {
+//         fontSize: 40,
+//         top: 0,
+//     },
+//     divider: {
+//         marginTop: 20,
+//         marginBottom: 20
+//     }
+// }));
