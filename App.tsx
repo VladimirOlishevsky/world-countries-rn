bf7c29edd70abe8ee6362983eb50fe2a860b35b8
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2020-12-01 00:20:12 +0400

    add mobx, app api

--- App.tsx
+++ App.tsx
@@ -1,15 +1,31 @@
 import React, { useState, useEffect, createContext, Fragment } from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { Navigation } from './src/Navigation';
+import storeInstance from './src/store/store';
+import { Provider } from "mobx-react";
 
-
-export default function App() {
-  return (
-    <Fragment>
-      <Navigation />
-    </Fragment>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider {...storeInstance}>
+         <Navigation />
+        {/* <SafeAreaView style={styles.safeArea}>
+          <Navigator style={styles.navigator} />
+        </SafeAreaView> */}
+      </Provider>
+    );
+  }
 }
+
+
+
+// export default function App() {
+//   return (
+//     <Fragment>
+//       <Navigation />
+//     </Fragment>
+//   );
+// }
 
 const styles = StyleSheet.create({
   image: {
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import {
     Text,
+    Button
 } from 'react-native';
+import {Observer, observer } from "mobx-react";
+
+import storeInstance from './store/store';
 
 
+export const MainComponent = observer((): JSX.Element => {
 
-
-export const MainComponent = () => {
     return (
-            <Text>777777777</Text>
+        <>
+            <Text>5555555555ff55555</Text>
+            <Button
+                title="Press me"
+
+                onPress={() => {
+                    storeInstance.fetchCountries('niger')
+                    console.log(storeInstance.countriesInfo)
+                    // return (
+                    //     //storeInstance.clearItem(),
+                    //     storeInstance.fetchCountries('egypt')
+                        
+                    // )
+                }}
+            />
+             <Text>52255445</Text>
+            <Text>{storeInstance.commentsCount}</Text>
+             <Text>77777777777</Text>
+        </>
+
     )
-}
+})
