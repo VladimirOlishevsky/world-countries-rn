35ef5959cb6933fd50a98b2b8b6f77a0c03e39b9
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2021-04-16 23:11:10 +0400

    add map marker on country page

--- src/Home/Country/index.tsx
+++ src/Home/Country/index.tsx
@@ -11,54 +11,64 @@ export const Country = observer(({ route }: any): JSX.Element => {
 
     const { countryStore } = getRootStore()
 
+    const isCoordinate = Boolean(countryStore.latlng[0] && countryStore.latlng[1])
+
     return (
         <View style={styles.scrollView}>
             <View style={styles.header}>
                 <BackButton />
             </View>
-            <Maps />
+
             <ScrollView >
-                {countryStore.errorMessage ? <Text>'sdfsdfsdf'</Text> : 
-                <>
-                <View style={styles.content}>
-                    <Image
-                        style={styles.image}
-                        source={{
-                            uri: countryStore.alpha2Code 
-                            ? `https://flagcdn.com/w640/${countryStore.alpha2Code.toLowerCase()}.png`
-                            : `https://flagcdn.com/w640/${route.params.name.toLowerCase()}.png` 
-                            
-                        }}
-                    />
-                    <Text style={styles.countryName}>
-                        {countryStore.name}
-                    </Text>
-                </View>
-                {Object.values(countryStore.store2Obj).map((el, index) => (
-                    <View key={index} style={styles.strings}>
-                        <Text style={styles.keys} >
-                            {countryStore.getKeyByValue(countryStore.store2Obj, el)}:
-                        </Text>
-                        {Array.isArray(el)
-                            ? (
-                                <View style={styles.column}>
-                                    {el.map((el: any) => (
-                                        <Text key={el.name} style={styles.values}>
-                                            {el.name}
-                                        </Text>
-                                    ))}
-                                </View>
-                            )
-                            :
-                            <Text key={el} style={styles.values}>
-                                {el}
+                {countryStore.errorMessage ? <Text>'sdfsdfsdf'</Text> :
+                    <>
+                        <View style={styles.content}>
+                            <Image
+                                style={styles.image}
+                                source={{
+                                    uri: countryStore.alpha2Code
+                                        ? `https://flagcdn.com/w640/${countryStore.alpha2Code.toLowerCase()}.png`
+                                        : `https://flagcdn.com/w640/${route.params.name.toLowerCase()}.png`
+
+                                }}
+                            />
+                            <Text style={styles.countryName}>
+                                {countryStore.name}
                             </Text>
-                        }
-                    </View>
-                    )
-                )}
-                </>
-}
+                        </View>
+                        {Object.values(countryStore.store2Obj).map((el, index) => (
+                            <View key={index} style={styles.strings}>
+                                <Text style={styles.keys} >
+                                    {countryStore.getKeyByValue(countryStore.store2Obj, el)}:
+                        </Text>
+                                {Array.isArray(el)
+                                    ? (
+                                        <View style={styles.column}>
+                                            {el.map((el: any) => (
+                                                <Text key={el.name} style={styles.values}>
+                                                    {el.name}
+                                                </Text>
+                                            ))}
+                                        </View>
+                                    )
+                                    :
+                                    <Text key={el} style={styles.values}>
+                                        {el}
+                                    </Text>
+                                }
+
+                            </View>
+                        )
+                        )}
+                    </>
+                }
+                {isCoordinate &&
+                    <Maps
+                        description={countryStore.name}
+                        latitude={countryStore.latlng[0]}
+                        longitude={countryStore.latlng[1]}
+                    />
+                }
             </ScrollView>
         </View>
     )
--- src/Home/Maps/index.tsx
+++ src/Home/Maps/index.tsx
@@ -1,17 +1,47 @@
 import React from 'react';
 import { observable } from 'mobx';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import { Text, View } from 'react-native';
 
-export const Maps = () => {
+interface IProps {
+    latitude: number,
+    longitude: number,
+    description: string
+}
+
+export const Maps = ({
+    latitude, longitude, description
+}: IProps) => {
     return (
         <MapView
-        style={{width: 200, height: 200}}
+            style={{ width: 200, height: 200 }}
             initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+                latitude: latitude,
+                longitude: longitude,
+                latitudeDelta: 10,
+                longitudeDelta: 10.05,
             }}
-        />
+        >
+             <Marker
+                //   onPress={() => handleNavigateToDetail(point.id)}
+                title={description}
+                  coordinate={{
+                    latitude: latitude,
+                    longitude: longitude,
+                  }}
+                >
+                     {/* <MapView.Callout> */}
+            {/* <View style={{height: 100, width: 200}}>
+              <Text> Title </Text>
+              <Text> Long Description    Even More Text</Text>
+              <Text>Multine Description </Text>
+            </View> */}
+          {/* </MapView.Callout> */}
+                     {/* <View style={{height: 20, width: 20}}>
+                         <Text>{description} </Text>
+                     </View> */}
+                     
+                </Marker>
+            </MapView>
     )
 }
